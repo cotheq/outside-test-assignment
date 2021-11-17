cd72a0d1744b0fa8887937d50c680025245c1b61
andrey kot <a.kot@scrile.com>
2021-11-17 22:17:28 +0700

    add base app styles

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <page-main />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import PageMain from "@/components/PageMain/PageMain.vue";
+import "@/styles/style.scss";
 
 export default defineComponent({
   name: "App",
   components: {
-    HelloWorld,
+    PageMain,
   },
 });
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: var(--gradient-red);
 }
 </style>
---
+++ src/components/PageMain/PageMain.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="page-main">
+    <button>Налоговый вычет</button>
+  </div>
+</template>
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "PageMain",
+  setup() {
+    console.log("test");
+  },
+});
+</script>
+<style lang="scss" scoped>
+.page-main {
+}
+</style>
